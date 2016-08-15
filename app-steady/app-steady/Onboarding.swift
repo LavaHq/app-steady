1af5c0c77f327baac257c0c43da28b611106942a
Daniel Habib <d.g.habib7@gmail.com>
2016-08-15 19:06:08 -0400

    Pod Install Paper On boarding

---
+++ app-steady/app-steady/Onboarding.swift
@@ -0,0 +1,22 @@
+//
+//  Onboarding.swift
+//  app-steady
+//
+//  Created by daniel.habib on 8/15/16.
+//  Copyright © 2016 Hawt-Lava. All rights reserved.
+//
+
+import UIKit
+
+class Onboarding: PaperOnboarding {
+
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
